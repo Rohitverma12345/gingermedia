b17bf8bb9a207a612de03d89c9a456afb98eaad0
Rohitverma12345 <122350697+Rohitverma12345@users.noreply.github.com>
2024-02-10 23:40:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Login from './Login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Signup from './Signup'
+
+function App() {
+  return (
+    <BrowserRouter>
+<Routes>
+
+  <Route path='/' element={<login/>}></Route>
+  <Route path='/signup' element={<Signup/>}></Route>
+</Routes>
+
+
+      <Login />
+    </BrowserRouter>
+  )
+}
+
+export default App
